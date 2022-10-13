717fdb9c2f2dbdaa12d53ae71f050d86298e7467
Davidnna <davedozie1@gmail.com>
2022-10-13 16:52:38 -0700

    Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 
 #include <stdio.h>
 
+/**
+ * main-Serves as the entry point for the program.
+ *
+ * Return: By default returns zero if no errors otherwise return non zero value.
+ */
+
 int main(void)
 
 {
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 
 #include <stdio.h>
 
+/**
+ * main-entry point for the program.
+ *
+ * Return:0 if no errors, return non zero if errors
+ */
+
 int main(void)
 
 {
