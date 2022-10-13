2219806e83e7d7d69dad968633862d8b65d2f908
Davidnna <davedozie1@gmail.com>
2022-10-13 16:33:15 -0700

    Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void)
+int main (void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
