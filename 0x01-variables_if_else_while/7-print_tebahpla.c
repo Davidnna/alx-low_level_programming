adb09019673bc7561cf9bb51c5d1b9176afb2e92
Davidnna <davedozie1@gmail.com>
2022-10-14 16:49:09 -0700

    DONE

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+/**
+ * main-program entry point
+ * Return:0 if no error, non-zero value if error
+ **/
+int main(void)
+{
+	int i;
+
+	for (i ='0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
