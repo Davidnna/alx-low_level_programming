761d4b19b16307f914e4ce0cba927cf9db16608b
Davidnna <davedozie1@gmail.com>
2022-10-19 10:02:56 -0700

    Done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -42,7 +42,7 @@ void print_times_table(int n)
 					_putchar((op % 10) + 48);
 				}
 			}
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
- * *separated by a comma followed by a space.
+ * * main - Lists all the natural numbers below 1024 (excluded)
+ * * that are multiples of 3 or 5.
  * *
  * * Return: Always 0.
  **/
@@ -10,22 +10,15 @@
 int main(void)
 
 {
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
+	int i, sum = 0;
 
-	for (count = 0; count < 50; count++)
+	for (i = 0; i < 1024; i++)
 	{
-		sum = fib1 + fib2;
-		printf("%lu", sum);
-
-		fib1 = fib2;
-		fib2 = sum;
-
-		if (count == 49)
-			printf("\n");
-		else
-			printf(", ");
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
 	}
+
+	printf("%d\n", sum);
 
 	return (0);
 }
