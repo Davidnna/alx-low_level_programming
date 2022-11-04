14fee07e37cede65e55e56fe4a8c44cdc452c22c
Davidnna <113899686+Davidnna@users.noreply.github.com>
2022-11-04 16:54:42 +0100

    Done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - function that creates an array of chars
+ * and initializes it with a specific char
+ *
+ * @c: array of chars
+ * @size: the size of the memory to print
+ *
+ * Return: address of the memory to print
+ */
