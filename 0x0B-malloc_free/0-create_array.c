97f8e6367f0f5e8302244bb99ae93a9fe6905162
Davidnna <113899686+Davidnna@users.noreply.github.com>
2022-11-04 16:56:02 +0100

    Done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,3 +12,26 @@
  *
  * Return: address of the memory to print
  */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *iArray;
+
+	if (size == 0)
+		return (NULL);
+
+/* returns a pointer to the allocated memory */
+	iArray = malloc(size * sizeof(char));
+
+	if (iArray == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < size)
+	{
+		iArray[i] = c;
+		i++;
+	}
+	return (iArray);
+}
