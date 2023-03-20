c9e5f8f3e96889ab099e863eb8b2d96256d188cf
Davidnna <davedozie1@gmail.com>
2023-03-20 11:11:38 -0700

    Done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp/ https://raw.github.com/Tecnophille/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
+export LD_PRELOAD=/tmp/preload.so
